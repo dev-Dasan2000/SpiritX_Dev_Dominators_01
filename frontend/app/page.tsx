72878fd343821e1127d0cbd84188e55bef7fd502
Dilesha Gayashan <m20010808008@student.cinec.edu>
2025-03-08 12:48:39 +0530

    comonent successdialogbox

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import SuccessDialog from '@/components/SuccessDailog';
+
 const page = () => {
   return (
-    <div>page</div>
+    
+      <div className="min-h-screen flex justify-center items-center">
+      <SuccessDialog
+        iconType="error"
+        message="Your success message"
+  subMessage="Your sub message"
+  showProgressBar={true} // Set to false to display the Yes/No buttons
+      />
+ 
+    </div>
   )
 }
 
